e3527caf0db657d2eec040394bc15bb35f2659be
Jenee <jenee.hughes@gmail.com>
2013-07-05 18:43:44 -0700

    Made find string in string func, passes tests

--- ScrabbleSuggester.java
+++ ScrabbleSuggester.java
@@ -3,6 +3,10 @@ import java.io.*;
 import java.lang.String;
 
 public class ScrabbleSuggester {
+   public static boolean isStringInString( String toFind, String str) {
+      System.err.println("Find "+toFind+" in "+str);
+      return str.contains(toFind);
+   }
    
    public static boolean isLetterInString( char letter, String str) {
       String toFind = String.valueOf(letter);
@@ -10,6 +14,8 @@ public class ScrabbleSuggester {
       System.err.println("Find "+toFind+" in "+str);
       return str.contains(toFind);
    }
+   
+   
    
    /*public static void main(String[] args) throws IOException {
 
--- ScrabbleSuggesterTester.java
+++ ScrabbleSuggesterTester.java
@@ -74,11 +74,94 @@ public class ScrabbleSuggesterTester {
       return cummRetVal;
    }
    
+   
+   /*tests to see whether a string exists in a string or not*/
+   public static boolean testIsStringInString() {
+      boolean cummRetVal = true;
+      boolean retVal = true;
+      boolean expected = true;
+      ScrabbleSuggester suggester = new ScrabbleSuggester();
+      String str1 = "hello";
+      String str2 = "What a label day!";
+      String toFind1 = "ha";
+      String toFind2 = "el";
+      String toFind3 = "hel";
+      
+      
+      System.err.println("------ test for " +toFind1+" in "+str1);
+      expected = false;
+      retVal = suggester.isStringInString(toFind1, str1);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      System.err.println("------ test for " +toFind2+" in "+str1);
+      expected = true;
+      retVal = suggester.isStringInString(toFind2, str1);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      System.err.println("------ test for " +toFind3+" in "+str1);
+      expected = true;
+      retVal = suggester.isStringInString(toFind3, str1);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      System.err.println("------ test for " +toFind1+" in "+str2);
+      expected = true;
+      retVal = suggester.isStringInString(toFind1, str2);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      System.err.println("------ test for " +toFind2+" in "+str2);
+      expected = true;
+      retVal = suggester.isStringInString(toFind2, str2);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      System.err.println("------ test for " +toFind3+" in "+str2);
+      expected = false;
+      retVal = suggester.isStringInString(toFind3, str2);
+      cummRetVal &= (expected == retVal);
+      System.err.println("------ expected: "+expected+";\tactual: "+retVal);
+      if( !cummRetVal ) {
+         return cummRetVal;
+      }
+      
+      return cummRetVal;
+   }
+   
+   
    public static boolean runTests() {
       boolean cummResult = true;
+      boolean result;
+      
       System.err.println("---Testing isLetterInString----");
-      boolean result = testIsLetterInString();
+      result = testIsLetterInString();
       if( testIsLetterInString() ) {
+         System.err.println("PASSED");
+      } else {
+         System.err.println("FAILED");
+      }
+      cummResult &= result;
+      
+      System.err.println("---Testing isStringInString----");
+      result = testIsStringInString();
+      if( testIsStringInString() ) {
          System.err.println("PASSED");
       } else {
          System.err.println("FAILED");
